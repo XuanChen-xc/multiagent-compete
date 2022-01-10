ea26027fb757d0d0ee182e6f1f4370a93be26ff5
chx1997 <chx1997@126.com>
2022-01-10 16:05:59 -0500

    fix bug

--- gym_compete/new_envs/multi_agent_scene.py
+++ gym_compete/new_envs/multi_agent_scene.py
@@ -27,8 +27,8 @@ class MultiAgentScene(mujoco_env.MujocoEnv):
         '''
         assert not self._mujoco_init, '_get_obs should not be called on Scene'
         obs = np.concatenate([
-            self.model.data.qpos.flat,
-            self.model.data.qvel.flat
+            self.sim.data.qpos.flat,
+            self.sim.data.qvel.flat
         ])
         return obs
 
